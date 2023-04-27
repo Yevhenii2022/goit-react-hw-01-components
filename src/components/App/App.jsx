6a24d863dcc75aaf0fc1b1d0a19a7e4e00a0cf80
Yevhenii2022 <3starosta3@gmail.com>
2023-04-27 11:53:07 +0300

    add

---
+++ src/components/App/App.jsx
@@ -0,0 +1,5 @@
+import { Container } from './App.styled';
+
+export const App = () => {
+  return <Container></Container>;
+};
---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  padding: 20px;
+`;
+
+// height: '100vh',
+// display: 'flex',
+// justifyContent: 'center',
+// alignItems: 'center',
+// fontSize: 40,
+// color: '#010101'
